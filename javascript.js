04a8883da932eeb74665334aeadc6ed9a3b0924a
RhettDelFierro <thinquetank@gmail.com>
2015-10-16 09:17:47 -0700

    Memory Match v0.5 - Rhett Del Fierro

--- javascript.js
+++ javascript.js
@@ -4,25 +4,24 @@
  var match_counter = 0;
 
  function card_clicked(cardClicked) {
-     $(cardClicked).hide();
+     $(cardClicked).hide(); //could put $(cardClicked).toggleClass('someclass that affects css') instead.
      if (first_card_clicked == null) {
          first_card_clicked = $(cardClicked).prev().find('img').attr('src'); //do I need to  get the image source for the DOM?
      } else {
          second_card_clicked = $(cardClicked).prev().find('img').attr('src'); //again, see above comment.
          if (first_card_clicked === second_card_clicked) {
              match_counter++;
-             console.log('match_counter is: ' + match_counter)
+             console.log('match_counter is: ' + match_counter);
              first_card_clicked = null;
              second_card_clicked = null;
              if (match_counter === total_possible_matches) {
                  alert('You have won!');
-
              } else {
                  return console.log('click handler functionality is complete - the first one');
              }
          } else {
              console.log('first_card_clicked != second_card_clicked');
-             $('.back').show(2000);
+             $('.back').show(2000); //could put $(cardClicked).toggleClass('someclass that affects css') instead to flip it back.
              first_card_clicked = null;
              second_card_clicked = null;
              console.log('click handler functionality is complete - the second');
