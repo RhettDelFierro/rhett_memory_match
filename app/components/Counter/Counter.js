7d7e68efe7781298b311997fe065bdb1db6f6d44
Rhett Del Fierro <thinquetank@gmail.com>
2016-07-20 17:21:54 -0700

    starting design

--- app/components/Counter/Counter.js
+++ app/components/Counter/Counter.js
@@ -1,8 +1,9 @@
 import React, {PropTypes} from 'react'
+import { container } from './styles.css'
 
 export default function Counter(props) {
     return (
-        <div onClick={props.startGame}>
+        <div className={container} onClick={props.startGame}>
             {!props.start ? <p>{'Click to begin'}</p>
                 : <div>
                 <p>Counter: <span>{props.attempts}/60 attempts</span></p>
