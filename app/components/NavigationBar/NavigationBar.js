b12a8b7833caebdb5a5d6987547a39ced8baee45
Rhett Del Fierro <thinquetank@gmail.com>
2016-08-11 22:23:00 -0700

    starting login page

--- app/components/NavigationBar/NavigationBar.js
+++ app/components/NavigationBar/NavigationBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 import { Nav, Navbar, NavItem } from "react-bootstrap"
 import { LoginFormContainer } from "containers"
 import { RegisterModalContainer } from "containers"
-import { container } from './styles.css'
+import { container, navContainer, link } from './styles.css'
 
 
 function RegisterToggle({UpdateLogin}) {
@@ -14,10 +14,28 @@ function RegisterToggle({UpdateLogin}) {
     )
 }
 
+function NavigationLinks({isAuthed}){
+    return isAuthed === true
+    ? <ul>
+        <li><Link to="/" className={link}>{'Home'}</Link></li>
+        <li><Link to="/memory_match" className={link}>{'Memory Match'}</Link></li>
+        <li><Link to="/perfect_pitch_training" className={link}>{'Perfect Pitch'}</Link></li>
+        <li><Link to="/scoreboard" className={link}>{'High Scores'}</Link></li>
+        <li><Link to="/profile" className={link}>{'Profile'}</Link></li>
+        <li><Link to="/practice" className={link}>{'Practice'}</Link></li>
+    </ul>
+        :<ul>
+        <li><Link to='/' className={link}>{'Home'}</Link></li>
+        <li><Link to='/login' className={link}>{'Login'}</Link></li>
+    </ul>
+}
+
 function NavigationBar({isAuthed, onUpdateLogin, user}) {
     return (
         <div className={container}>
-            some blah blah
+            <nav className={navContainer}>
+                <NavigationLinks isAuthed={isAuthed}/>
+            </nav>
         </div>
     )
 }
