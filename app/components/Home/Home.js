f25a95a0b058bb9766a6082f5d8a0b4fd255e7f3
Rhett Del Fierro <thinquetank@gmail.com>
2016-09-16 03:38:04 -0700

    going to make own scroll events.

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router"
 import { container, header, background,example, example2, examplea } from './styles.css'
-import { Parallax } from 'react-parallax'
+import { Parallax, Background } from 'react-parallax'
 
 
 //remember to use refs you must use a container component, it will not take a functional component.
@@ -9,16 +9,13 @@ function Home() {
     const img = require('../../assets/images/backgroundattempt2.jpg')
     return (
         <div className={container}>
-            <Parallax className={background} bgHeight="5000px" bgImage={img} strength={200}>
-                <div className={header}></div>
-                <h1>blah</h1>
-                <h1>blah</h1>
-                <div className={example}>Blah haha!</div>
-                <h1>blah</h1>
-                <div className={examplea}>Blah haaha!</div>
-                <h1>blah</h1>
-                <div className={example2}>Blah haha!</div>
-            </Parallax>
+            <div className={header}></div>
+
+            <h1>blah</h1>
+
+            <div className={examplea}>Blah haaha!</div>
+            <h1>blah</h1>
+            <div className={example2}>Blah haha!</div>
         </div>
     )
 
