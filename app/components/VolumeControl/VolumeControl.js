884b9a40ae47e723acebee1e1f53e2dc9775a9a3
Rhett Del Fierro <thinquetank@gmail.com>
2016-09-27 15:38:07 -0700

    updated the VolumeControl component to connect with Redux and update it's node's values on render. Need to build mapdispatchtoprops and make th enecessary imports.

--- app/components/VolumeControl/VolumeControl.js
+++ app/components/VolumeControl/VolumeControl.js
@@ -5,6 +5,20 @@ import { container, targetNote, noiseVolume, maskingNotes } from './styles.css'
 class VolumeControl extends Component {
     constructor() {
         super()
+        this.state = {
+            targetNote: this.props.targetNoteVolume,
+            noise: this.props.noiseVolume,
+            maskingNotes: this.props.maskingNotesVolume
+
+        }
+    }
+    
+    handleUpdate() {
+        this.props.setVolume({
+            targetNoteVolume: this.targetNoteNode.value,
+            noiseVolume: this.noiseVolumeNode.value,
+            maskingNotesVolume: this.maskingNotesNode.value
+        })
     }
 
     render() {
@@ -12,25 +26,25 @@ class VolumeControl extends Component {
         //when the component mounts we want to render the volume again with the right position.
 
         return (
-            <div ref={this.props.volumeRef}>
+            <div>
                 <input className={targetNote} ref={ref => this.targetNoteNode = ref} type="range" min="0" max="9"
                        step="0.01" value={this.props.targetNote} disabled={this.props.onCheck}
-                       onMouseUp={this.props.onUpdate} onTouchEnd={this.props.onUpdate}/>
+                       onMouseUp={this.handleUpdate} onTouchEnd={this.handleUpdate}/>
 
                 <input className={noiseVolume} ref={ref => this.noiseVolumeNode = ref} type="range" min="0" max="9"
                        step="0.01" value={this.props.noise} disabled={this.props.onCheck}
-                       onMouseUp={this.props.onUpdate} onTouchEnd={this.props.onUpdate}/>
+                       onMouseUp={this.handleUpdate} onTouchEnd={this.handleUpdate}/>
 
                 <input className={maskingNotes} ref={ref => this.maskingNotesNode = ref} type="range" min="0" max="9"
                        step="0.01" value={this.props.maskingNotes} disabled={this.props.onCheck}
-                       onMouseUp={this.props.onUpdate} onTouchEnd={this.props.onUpdate}/>
+                       onMouseUp={this.handleUpdate} onTouchEnd={this.handleUpdate}/>
             </div>
         )
     }
 }
 
 //function Volume(props) {
-//    return <input type="range" min="0" max="9" step="0.01" disabled={this.props.onCheck} onChange={this.props.onUpdate}/>
+//    return <input type="range" min="0" max="9" step="0.01" disabled={this.props.onCheck} onChange={this.props.handleUpdate}/>
 //}
 
 const { func, bool } = PropTypes
